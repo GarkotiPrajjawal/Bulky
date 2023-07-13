459f55df6efbc338b841f6c584e26fc32c61df26
prajjawal.garkoti <prajjawal.garkoti@KELLGGNLPTP1589>
2023-07-13 05:12:11 -0700

    Add project files.

---
+++ Bulky_Web/Controllers/CategoryController.cs
@@ -0,0 +1,39 @@
+﻿using Bulky_Web.Data;
+using Bulky_Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bulky_Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
