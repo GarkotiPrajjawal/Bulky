619416a5ce1bd51cee5e57574a88be0abef51fc9
prajjawalgarkoti8300@gmail.com <prajjawalgarkoti8300@gmail.com>
2023-07-16 05:38:13 -0700

    fifth

--- Bulky_Web/Program.cs
+++ Bulky_Web/Program.cs
@@ -1,11 +1,14 @@
 
-using Bulky.DataAcess.Data;
+using BulkyBook.DataAccess.Repository.iRepository;
+using BulkyBook.DataAccess.Repository;
+using BulkyBook.DataAcess.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -29,6 +32,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
